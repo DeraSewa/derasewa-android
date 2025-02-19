4eb889776896969ac0b21c696dbc1b0b19408f83
devRajeshThapa <devrajeshthapa@gmail.com>
2025-02-19 15:07:09 +0545

    Add bottom nav and redirect user to login if if isLoggedIn value in shared preference is false

--- app/src/main/java/com/example/derasewa/MainActivity.java
+++ app/src/main/java/com/example/derasewa/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.derasewa;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Check login status before initializing UI
+        SharedPreferences sharedPreferences = getSharedPreferences("user_preferences", MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);  // Default is false
+
+        if (!isLoggedIn) {
+            // User is not logged in; redirect to LoginActivity
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();  // Prevent user from returning to this activity
+            return;
+        }
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +43,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id == R.id.nav_home){
+                    loadFragment(new HomeFragment());
+                }else if(id == R.id.nav_profile){
+                    loadFragment(new ProfileFragment());
+                }else if(id == R.id.nav_setting){
+                    loadFragment(new SettingFragment());
+                }
+
+                return true;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId((R.id.nav_home));
+    }
+
+    public  void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
